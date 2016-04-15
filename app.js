8efed2ace0f86638f26154cd1bb7a883517c2221
mbthoms <mbthoms@outlook.com>
2016-04-15 08:09:59 -0400

    first-commit

---
+++ app.js
@@ -0,0 +1,48 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var routes = require('./routes/index');
+var users = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+app.use('/users', users);
+
+//Step 2 - Catching the 404 and forwarding to the error handler.
+
+
+
+
+
+
+//Step 3 - Error handing.
+// This will print the stacktrace.
+
+
+
+
+
+
+//Step 4
+//This is the production error handler, it makes sure the stacktraces are not leaked to user
+
+
+
+module.exports = app;
